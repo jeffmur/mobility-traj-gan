7e5043c18ef302bfc0fcc2af6754d512d2450384
Alex Kyllo <alex.kyllo@gmail.com>
2021-06-19 00:03:43 -0700

    gan training loop work

--- src/models/gan.py
+++ src/models/gan.py
@@ -7,8 +7,11 @@ import os
 from datetime import datetime
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import Model, initializers, layers, losses, regularizers
+from tensorflow.keras import (Model, initializers, layers, losses, optimizers,
+                              regularizers, utils)
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 SEED = 11
 
@@ -32,7 +35,7 @@ def build_inputs_latlon(timesteps, dense_units):
 
 def build_inputs_cat(timesteps, levels, feature_name):
     """Build input layers for categorical features."""
-    i = layers.Input(shape=(timesteps, levels), name="input_" + feature_name)
+    i = layers.Input(shape=(timesteps, levels), name=f"input_{feature_name}")
     mask = layers.Masking()(i)
     unstacked = layers.Lambda(lambda x: tf.unstack(x, axis=1))(mask)
     d = layers.Dense(
@@ -47,16 +50,19 @@ def build_inputs_cat(timesteps, levels, feature_name):
     return (i, e)
 
 
-def build_inputs(timesteps, latlon_dense_units, concat_dense_units, latent_dim, noise=False):
+def build_inputs(
+    timesteps, latlon_dense_units, concat_dense_units, latent_dim, vocab_sizes, noise=False
+):
     """Build the multiple input layer for both the generator and the discriminator."""
     latlon_input, latlon_embed = build_inputs_latlon(timesteps, latlon_dense_units)
-    day_input, day_embed = build_inputs_cat(timesteps, 7, "day")
-    hour_input, hour_embed = build_inputs_cat(timesteps, 24, "hour")
-    inputs = [latlon_input, day_input, hour_input]
-    embeddings = [latlon_embed, day_embed, hour_embed]
+    inputs = [latlon_input]
+    embeddings = [latlon_embed]
+    for key, val in vocab_sizes.items():
+        cat_input, cat_embed = build_inputs_cat(timesteps, val, key)
+        inputs.append(cat_input)
+        embeddings.append(cat_embed)
     concat_input = layers.Concatenate(axis=2)(embeddings)
     unstacked = layers.Lambda(lambda x: tf.unstack(x, axis=1))(concat_input)
-
     d = layers.Dense(
         units=concat_dense_units,
         activation="relu",
@@ -80,55 +86,49 @@ def build_generator(
     lstm_units,
     latent_dim,
     lstm_reg,
+    vocab_sizes,
 ):
     """Build the generator network."""
     # Add random noise input
     inputs, emb_traj = build_inputs(
-        timesteps, latlon_dense_units, concat_dense_units, latent_dim, noise=True
+        timesteps, latlon_dense_units, concat_dense_units, latent_dim, vocab_sizes, noise=True
     )
-
     lstm_cell = layers.LSTM(
         units=lstm_units,
         batch_input_shape=(None, timesteps, latent_dim),
         return_sequences=True,
         recurrent_regularizer=regularizers.l1(lstm_reg),
     )(emb_traj)
-
     latlon_output = layers.TimeDistributed(
         layers.Dense(2, activation="tanh"), name="output_latlon"
     )(lstm_cell)
-
-    day_output = layers.TimeDistributed(layers.Dense(7, activation="softmax"), name="output_day")(
-        lstm_cell
-    )
-
-    hour_output = layers.TimeDistributed(
-        layers.Dense(24, activation="softmax"), name="output_hour"
-    )(lstm_cell)
-    outputs = [latlon_output, day_output, hour_output]
+    outputs = [latlon_output]
+    for key, val in vocab_sizes.items():
+        out = layers.TimeDistributed(layers.Dense(val, activation="softmax"), name=f"output_{key}")(
+            lstm_cell
+        )
+        outputs.append(out)
     return Model(inputs=inputs, outputs=outputs, name="generator")
 
 
 def build_discriminator(
-    timesteps, latlon_dense_units, concat_dense_units, lstm_units, latent_dim, lstm_reg
+    timesteps, latlon_dense_units, concat_dense_units, lstm_units, latent_dim, lstm_reg, vocab_sizes
 ):
     """Build the discriminator network."""
-    inputs, emb_traj = build_inputs(timesteps, latlon_dense_units, concat_dense_units, latent_dim)
-
-    # LSTM Modeling Layer (many-to-one)
+    inputs, emb_traj = build_inputs(
+        timesteps, latlon_dense_units, concat_dense_units, latent_dim, vocab_sizes
+    )
     lstm_cell = layers.LSTM(units=lstm_units, recurrent_regularizer=regularizers.l1(lstm_reg))(
         emb_traj
     )
-
-    # Output
     output = layers.Dense(1, activation="sigmoid")(lstm_cell)
-
     return Model(inputs=inputs, outputs=output, name="discriminator")
 
 
 def build_gan(
     optimizer,
     timesteps,
+    vocab_sizes,
     latlon_dense_units=64,
     concat_dense_units=100,
     lstm_units=100,
@@ -137,10 +137,22 @@ def build_gan(
 ):
     """Build the full GAN network"""
     gen = build_generator(
-        timesteps, latlon_dense_units, concat_dense_units, lstm_units, latent_dim, lstm_reg
+        timesteps,
+        latlon_dense_units,
+        concat_dense_units,
+        lstm_units,
+        latent_dim,
+        lstm_reg,
+        vocab_sizes,
     )
     dis = build_discriminator(
-        timesteps, latlon_dense_units, concat_dense_units, lstm_units, latent_dim, lstm_reg
+        timesteps,
+        latlon_dense_units,
+        concat_dense_units,
+        lstm_units,
+        latent_dim,
+        lstm_reg,
+        vocab_sizes,
     )
     dis.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
     dis.trainable = False
@@ -151,31 +163,11 @@ def build_gan(
         loss=[
             "binary_crossentropy",  # discriminator real/fake trajectory predictions
             "mean_squared_error",  # lat-lon predictions
-            "categorical_crossentropy",  # day of week predictions
-            "categorical_crossentropy",  # hour of day predictions
+            *["categorical_crossentropy" for _ in vocab_sizes],
         ],
         loss_weights=[1.0, 10.0, 1.0, 1.0],  # 10x weight on lat-lon loss
     )
     return gen, dis, gan
-
-
-def draw_real(dataset, n, channels: int = 1):
-    """Draw a batch of random real images from the dataset."""
-    idxs = np.random.permutation(dataset.shape[0])[0:n]
-    y = np.ones((n, 1))
-    return dataset[idxs], y
-
-
-def draw_z(latent_dim, n):
-    """Draw points from the latent space."""
-    return np.random.randn(latent_dim * n).reshape(n, latent_dim)
-
-
-def draw_fake(generator, inputs, latent_dim: int, n: int):
-    """Draw a batch of fake images from the latent space."""
-    x = generator.predict(inputs)
-    y = np.zeros((n, 1))
-    return x, y
 
 
 def write_csv(exp_name, start_time, epoch, **kwargs):
@@ -195,42 +187,65 @@ def write_csv(exp_name, start_time, epoch, **kwargs):
             writer.writerow(row)
 
 
-def train(exp_name, gen, dis, gan, x_train, x_valid, epochs=200, batch_size=256, latent_dim=100):
+def split_inputs(x, vocab_sizes):
+    """Split NumPy array into separate feature arrays."""
+    x_split = [x[:, :, 0:2]]  # lat-lon feature
+    start = 2
+    for key, val in vocab_sizes.items():
+        x_split.append(x[:, :, start : (start + val)])
+        start = start + val
+    return x_split
+
+
+def train(
+    exp_name,
+    gen,
+    dis,
+    gan,
+    x_train,
+    x_valid,
+    vocab_sizes,
+    epochs=200,
+    batch_size=32,
+    latent_dim=100,
+):
     """Train the GAN."""
     start_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     n_examples = x_train.shape[0]
     n_batches = np.ceil(n_examples / batch_size).astype(int)
+    random_idx = np.random.permutation(x_train.shape[0])
     for i in range(1, epochs + 1):
         for j in range(1, n_batches + 1):
-            x_real, y_real = draw_real(x_train, batch_size // 2)
-
+            idx = random_idx[batch_size * i : batch_size * (i + 1)]
+            x_real = x_train[idx]
+            y_real = np.ones((idx.shape[0], 1))
             # split the x variables into separate input arrays.
             # lat-lon = 2 features
             # one-hot encoded day of week = 7 features
             # one-hot encoded hour of day = 24 features
-            x_split = [x_real[:, :, 0:2], x_real[:, :, 2:9], x_real[:, :, 9:]]
-
-            # generate synthetic examples
-            gen_input = draw_z(latent_dim, batch_size)  # TODO: check this is the right size
-            x_fake, y_fake = draw_fake(gen, [*x_split, gen_input], latent_dim, batch_size // 2)
+            x_split = split_inputs(x_real, vocab_sizes)
+            # generator input is conditioned on real data with addition of Gaussian noise
+            noise = np.random.normal(0, 1, (idx.shape[0], latent_dim))
+            x_fake = np.concatenate(gen.predict([*x_split, noise]), axis=2)
+            y_fake = np.zeros((batch_size, 1))
+            # shuffle the real and synthetic examples together
             x = np.vstack((x_real, x_fake))
             y = np.vstack((y_real, y_fake))
-
-            # shuffle the real and synthetic examples together
-            idx = np.random.permutation(x.shape[0])
-            x = x[idx]
-            y = y[idx]
-
+            idx_shuf = np.random.permutation(x.shape[0])
+            x = x[idx_shuf]
+            y = y[idx_shuf]
+            x_split = split_inputs(x, vocab_sizes)
             # train the discriminator
-            dis_res = dis.train_on_batch(x, y, return_dict=True)
+            dis_res = dis.train_on_batch(x_split, y, return_dict=True)
             dis_loss = dis_res["loss"]
             dis_acc = dis_res["accuracy"]
-
             # train the combined model
-            gen_labels = np.ones((batch_size, 1))
+            gen_labels = np.ones((batch_size * 2, 1))
             # "one-sided label smoothing": use 0.9 instead of 1 as real labels
             # gen_labels = np.full((batch_size, 1), 0.9)
-            gen_loss = gan.train_on_batch(gen_input, gen_labels)
+            noise = np.random.normal(0, 1, (x.shape[0], latent_dim))
+            # TODO: get the shapes right for loss function calculation
+            gen_loss = gan.train_on_batch([*x_split, noise], [gen_labels, *x_split])
             print(
                 " ".join(
                     f"""Epoch {i}/{epochs}, Batch {j}/{n_batches}, dis_loss={dis_loss:.3f},
@@ -240,29 +255,70 @@ def train(exp_name, gen, dis, gan, x_train, x_valid, epochs=200, batch_size=256,
                 end="\r",
             )
         print()
-        x_valid_real, y_valid_real = draw_real(x_valid, batch_size // 2)
-        x_valid_split = [x_valid_real[:, :, 0:2], x_valid_real[:, :, 2:9], x_valid_real[:, :, 9:]]
-        gen_valid_input = draw_z(latent_dim, batch_size // 2)
-        x_valid_fake, y_valid_fake = draw_fake(
-            gen, [*x_valid_split, gen_valid_input], latent_dim, batch_size // 2
-        )
-        dis_valid_r_loss, dis_valid_r_acc = dis.evaluate(x_valid_real, y_valid_real)
-        dis_valid_f_loss, dis_valid_f_acc = dis.evaluate(x_valid_fake, y_valid_fake)
-        print(
-            " ".join(
-                f"""Experiment {exp_name} epoch{i:04d}
-                loss real: {dis_valid_r_loss:.3f} fake: {dis_valid_f_loss:.3f}
-                accuracy real: {dis_valid_r_acc:.2f}, fake: {dis_valid_f_acc:.2f}""".splitlines()
-            )
-        )
-        rounding = 3
-        # log learning curves to a CSV file.
-        write_csv(
-            exp_name,
-            start_time,
-            i,
-            val_discriminator_accuracy_real=round(dis_valid_r_acc, rounding),
-            val_discriminator_loss_real=round(dis_valid_r_loss, rounding),
-            val_discriminator_accuracy_fake=round(dis_valid_f_acc, rounding),
-            val_discriminator_loss_fake=round(dis_valid_f_loss, rounding),
-        )
+        # validation at end of epoch
+        # y_val_real = np.ones((x_valid.shape[0], 1))
+        # y_val_fake = np.zeros((x_valid.shape[0], 1))
+        # x_val_split = split_inputs(x_valid, vocab_sizes)
+        # noise = np.random.normal(0, 1, (x_valid.shape[0], latent_dim))
+        # gen_input = [*x_val_split, noise]
+        # x_val_fake = gen.predict(gen_input)
+        # dis_val_r_loss, dis_valid_r_acc = dis.evaluate(x_val_split, y_val_real)
+        # dis_valid_f_loss, dis_valid_f_acc = dis.evaluate(x_val_fake, y_val_fake)
+        # print(
+        #     " ".join(
+        #         f"""Experiment {exp_name} epoch{i:04d}
+        #         loss real: {dis_val_r_loss:.3f} fake: {dis_valid_f_loss:.3f}
+        #         accuracy real: {dis_valid_r_acc:.2f}, fake: {dis_valid_f_acc:.2f}""".splitlines()
+        #     )
+        # )
+        # # TODO: add early stopping
+        # gen.save(f"experiments/{exp_name}/{start_time}/{i:04d}")
+        # rounding = 3
+        # # log learning curves to a CSV file.
+        # write_csv(
+        #     exp_name,
+        #     start_time,
+        #     i,
+        #     val_discriminator_accuracy_real=round(dis_valid_r_acc, rounding),
+        #     val_discriminator_loss_real=round(dis_val_r_loss, rounding),
+        #     val_discriminator_accuracy_fake=round(dis_valid_f_acc, rounding),
+        #     val_discriminator_loss_fake=round(dis_valid_f_loss, rounding),
+        # )
+
+
+def run():
+    """Run an experiment with a given set of hyperparameters."""
+    os.makedirs("experiments", exist_ok=True)
+    exp_name = "000"
+    optimizer = optimizers.Adam(0.001, 0.5)
+    timesteps = 144
+    vocab_sizes = {"day": 7, "hour": 24, "category": 10}
+    gen, dis, gan = build_gan(optimizer, timesteps, vocab_sizes)
+    # from CSV
+    # df = pd.read_csv("data/dev_train_encoded_final.csv")
+    # cat_frames = []
+    # for key, val in vocab_sizes.items():
+    #     cat = pd.DataFrame(
+    #         utils.to_categorical(df[key], num_classes=val),
+    #         columns=[f"{key}_{i}" for i in range(0, val)],
+    #     )
+    #     cat_frames.append(cat)
+    # df = pd.concat([df.drop(["tid", "label", *vocab_sizes.keys()], axis=1), *cat_frames], axis=1)
+    # x = df.to_numpy()
+
+    ## From saved npy
+    x = np.load("data/final_train.npy", allow_pickle=True)
+    x = x[0 : (len(vocab_sizes) + 1)]
+    # Padding zero to reach the maxlength
+    x = np.concatenate(
+        [pad_sequences(f, timesteps, padding="post", dtype="float64") for f in x], axis=2
+    )
+
+    n = x.shape[0]
+    idx = np.random.permutation(n)
+    valid_split = 0.1
+    valid_n = np.ceil(n * valid_split).astype(int)
+    valid_idx, train_idx = idx[:valid_n], idx[valid_n:]
+    x_train, x_valid = x[train_idx, :], x[valid_idx, :]
+    train(exp_name, gen, dis, gan, x_train, x_valid, vocab_sizes)
+    return gen, dis, gan
