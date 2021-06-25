bca15ed04e88fb2a4360c054ca0dec3659fae2c6
Alex Kyllo <alex.kyllo@gmail.com>
2021-06-24 23:35:43 -0700

    working on loss function symbolic tensor issue

--- src/models/gan.py
+++ src/models/gan.py
@@ -7,18 +7,51 @@ import os
 from datetime import datetime
 
 import numpy as np
-import pandas as pd
 import tensorflow as tf
-from tensorflow.keras import Model, initializers, layers, losses, optimizers, regularizers, utils
+from tensorflow.keras import Model
+from tensorflow.keras import backend as K
+from tensorflow.keras import initializers, layers, losses, optimizers, regularizers
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 SEED = 11
+
+# Masked Loss from LSTM-TrajGAN
+def traj_loss(real_traj, gen_traj, mask, vocab_sizes, latlon_weight=10):
+    """Novel trajectory loss from LSTM-TrajGAN paper"""
+
+    def loss(y_true, y_pred):
+        traj_length = K.sum(mask, axis=1)
+        bce_loss = losses.binary_crossentropy(y_true, y_pred)
+        masked_latlon_full = K.sum(
+            K.sum(
+                tf.multiply(
+                    tf.multiply((gen_traj[0] - real_traj[0]), (gen_traj[0] - real_traj[0])),
+                    tf.concat([mask for x in range(2)], 2),
+                ),
+                axis=1,
+            ),
+            axis=1,
+            keepdims=True,
+        )
+        masked_latlon_mse = K.sum(tf.math.divide(masked_latlon_full, traj_length))
+        cat_losses = []
+        for idx in range(1, len(vocab_sizes) + 1):
+            ce_loss = tf.compat.v1.nn.softmax_cross_entropy_with_logits_v2(
+                gen_traj[idx], real_traj[idx]
+            )
+            ce_loss_masked = tf.multiply(ce_loss, K.sum(mask, axis=2))
+            ce_mean = K.sum(tf.math.divide(ce_loss_masked, traj_length))
+            cat_losses.append(ce_mean)
+
+        total_loss = bce_loss + masked_latlon_mse * latlon_weight + K.sum(cat_losses)
+        return total_loss
+
+    return loss
 
 
 def build_inputs_latlon(timesteps, dense_units):
     """Build input layers for lat-lon features."""
     i = layers.Input(shape=(timesteps, 2), name="input_latlon")
-    # TODO: check that masking is being propagated to the losses.
     mask = layers.Masking()(i)
     unstacked = layers.Lambda(lambda x: tf.unstack(x, axis=1), mask)(mask)
     d = layers.Dense(
@@ -51,7 +84,13 @@ def build_inputs_cat(timesteps, levels, feature_name):
 
 
 def build_inputs(
-    timesteps, latlon_dense_units, concat_dense_units, latent_dim, vocab_sizes, noise=False
+    timesteps,
+    latlon_dense_units,
+    concat_dense_units,
+    latent_dim,
+    vocab_sizes,
+    noise=False,
+    mask=False,
 ):
     """Build the multiple input layer for both the generator and the discriminator."""
     latlon_input, latlon_embed = build_inputs_latlon(timesteps, latlon_dense_units)
@@ -62,7 +101,6 @@ def build_inputs(
         inputs.append(cat_input)
         embeddings.append(cat_embed)
     concat_input = layers.Concatenate(axis=2)(embeddings)
-    masking_fn = lambda _, prev_mask: tf.unstack(prev_mask, axis=1)
     unstacked = layers.Lambda(lambda x: tf.unstack(x, axis=1))(concat_input)
     d = layers.Dense(
         units=concat_dense_units,
@@ -76,6 +114,8 @@ def build_inputs(
         dense_outputs = [d(layers.Concatenate(axis=1)([x, noise_input])) for x in unstacked]
     else:
         dense_outputs = [d(x) for x in unstacked]
+    if mask:
+        inputs.append(layers.Input(shape=(timesteps, 1), name="input_mask"))
     emb_traj = layers.Lambda(lambda x: tf.stack(x, axis=1))(dense_outputs)
     return (inputs, emb_traj)
 
@@ -92,7 +132,13 @@ def build_generator(
     """Build the generator network."""
     # Add random noise input
     inputs, emb_traj = build_inputs(
-        timesteps, latlon_dense_units, concat_dense_units, latent_dim, vocab_sizes, noise=True
+        timesteps,
+        latlon_dense_units,
+        concat_dense_units,
+        latent_dim,
+        vocab_sizes,
+        noise=True,
+        mask=True,
     )
     lstm_cell = layers.LSTM(
         units=lstm_units,
@@ -109,6 +155,8 @@ def build_generator(
             lstm_cell
         )
         outputs.append(out)
+    # pass the mask through so the loss function can use it
+    outputs.append(layers.Lambda(lambda x: x, name="output_mask")(inputs[-1]))
     return Model(inputs=inputs, outputs=outputs, name="generator")
 
 
@@ -155,18 +203,26 @@ def build_gan(
         lstm_reg,
         vocab_sizes,
     )
+    # Compile discriminator with masked BCE loss. Mask is last output of generator
     dis.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
     dis.trainable = False
-    pred = dis(gen.outputs)
-    gan = Model(inputs=gen.inputs, outputs=[pred, *(gen.outputs)])
+
+    # The trajectory generator takes real trajectories and noise as inputs
+    inputs = [layers.Input(shape=(timesteps, 2), name="input_latlon")]
+    for key, val in vocab_sizes.items():
+        inputs.append(layers.Input(shape=(timesteps, val), name="input_" + key))
+    inputs.append(layers.Input(shape=(latent_dim,), name="input_noise"))
+    inputs.append(layers.Input(shape=(timesteps, 1), name="input_mask"))
+
+    # The trajectory generator generates synthetic trajectories
+    gen_trajs = gen(inputs)
+
+    pred = dis(gen_trajs[:-1])  # discriminator does not take the mask as input
+    gan = Model(inputs, pred)
+    mask = inputs[-1]
     gan.compile(
         optimizer=optimizer,
-        loss=[
-            "binary_crossentropy",  # discriminator real/fake trajectory predictions
-            "mean_squared_error",  # lat-lon predictions
-            *["categorical_crossentropy" for _ in vocab_sizes],
-        ],
-        loss_weights=[1.0, 10.0, *[1.0 for _ in vocab_sizes]],  # 10x weight on lat-lon loss
+        loss=traj_loss(inputs[:-2], gen_trajs[:-1], inputs[-1], vocab_sizes),
     )
     return gen, dis, gan
 
@@ -192,9 +248,11 @@ def split_inputs(x, vocab_sizes):
     """Split NumPy array into separate feature arrays."""
     x_split = [x[:, :, 0:2]]  # lat-lon feature
     start = 2
-    for key, val in vocab_sizes.items():
+    for val in vocab_sizes.values():
         x_split.append(x[:, :, start : (start + val)])
         start = start + val
+    # append mask
+    x_split.append(x[:, :, [-1]])
     return x_split
 
 
@@ -229,17 +287,20 @@ def train(
             x_split = split_inputs(x_real, vocab_sizes)
             # generator input is conditioned on real data with addition of Gaussian noise
             noise = np.random.normal(0, 1, (this_batch_size, latent_dim))
-            x_fake = np.concatenate(gen.predict([*x_split, noise]), axis=2)
+            # pass generations (dropping input mask) to discriminator
+            gen_inputs = x_split
+            gen_inputs.insert(-1, noise)
+            x_fake = np.concatenate(gen.predict(gen_inputs)[:-1], axis=2)
             y_fake = np.zeros((this_batch_size, 1))
-            # shuffle the real and synthetic examples together
-            x = np.vstack((x_real, x_fake))
+            # shuffle the real and synthetic examples together, removing the mask
+            x = np.vstack((x_real[:, :, :-1], x_fake))
             y = np.vstack((y_real, y_fake))
             idx_shuf = np.random.permutation(x.shape[0])
             x = x[idx_shuf]
             y = y[idx_shuf]
             x_split = split_inputs(x, vocab_sizes)
             # train the discriminator
-            dis_res = dis.train_on_batch(x_split, y, return_dict=True)
+            dis_res = dis.train_on_batch(x_split[:-1], y, return_dict=True)
             dis_loss = dis_res["loss"]
             dis_acc = dis_res["accuracy"]
             # train the combined model
@@ -247,8 +308,10 @@ def train(
             # "one-sided label smoothing": use 0.9 instead of 1 as real labels
             # gen_labels = np.full((batch_size, 1), 0.9)
             noise = np.random.normal(0, 1, (this_batch_size * 2, latent_dim))
+            gen_inputs = x_split
+            gen_inputs.insert(-1, noise)
             gen_loss = gan.train_on_batch(
-                [*x_split, noise], [gen_labels, *x_split], return_dict=True
+                gen_inputs, [gen_labels, *x_split[:-2]], return_dict=True
             )["loss"]
             print(
                 " ".join(
@@ -260,41 +323,41 @@ def train(
             )
         print()
         # validation at end of epoch
-        y_val_real = np.ones((x_valid.shape[0], 1))
-        y_val_fake = np.zeros((x_valid.shape[0], 1))
-        x_val_split = split_inputs(x_valid, vocab_sizes)
-        noise = np.random.normal(0, 1, (x_valid.shape[0], latent_dim))
-        gen_input = [*x_val_split, noise]
-        gen_val_labels = np.ones((x_valid.shape[0], 1))
-        gen_val_loss = gan.evaluate(gen_input, [gen_val_labels, *x_val_split], return_dict=True)[
-            "loss"
-        ]
-        x_val_fake = gen.predict(gen_input)
-        dis_val_r_loss, dis_valid_r_acc = dis.evaluate(x_val_split, y_val_real)
-        dis_valid_f_loss, dis_valid_f_acc = dis.evaluate(x_val_fake, y_val_fake)
-        print(
-            " ".join(
-                f"""Experiment {exp_name} epoch{i:04d}
-                G val loss: {gen_val_loss:.3f}
-                D val loss real: {dis_val_r_loss:.3f} fake: {dis_valid_f_loss:.3f}
-                D val accuracy real: {dis_valid_r_acc:.2f}, fake: {dis_valid_f_acc:.2f}""".splitlines()
-            )
-        )
-        # TODO: add logging
-        # TODO: add early stopping
-        if i % save_interval == 0:
-            gen.save(f"experiments/{exp_name}/{start_time}/{i:04d}")
-        rounding = 3
-        # log learning curves to a CSV file.
-        write_csv(
-            exp_name,
-            start_time,
-            i,
-            val_discriminator_accuracy_real=round(dis_valid_r_acc, rounding),
-            val_discriminator_loss_real=round(dis_val_r_loss, rounding),
-            val_discriminator_accuracy_fake=round(dis_valid_f_acc, rounding),
-            val_discriminator_loss_fake=round(dis_valid_f_loss, rounding),
-        )
+        # y_val_real = np.ones((x_valid.shape[0], 1))
+        # y_val_fake = np.zeros((x_valid.shape[0], 1))
+        # x_val_split = split_inputs(x_valid, vocab_sizes)
+        # noise = np.random.normal(0, 1, (x_valid.shape[0], latent_dim))
+        # gen_input = [*x_val_split, noise]
+        # gen_val_labels = np.ones((x_valid.shape[0], 1))
+        # gen_val_loss = gan.evaluate(gen_input, [gen_val_labels, *x_val_split], return_dict=True)[
+        #     "loss"
+        # ]
+        # x_val_fake = gen.predict(gen_input)
+        # dis_val_r_loss, dis_valid_r_acc = dis.evaluate(x_val_split, y_val_real)
+        # dis_valid_f_loss, dis_valid_f_acc = dis.evaluate(x_val_fake, y_val_fake)
+        # print(
+        #     " ".join(
+        #         f"""Experiment {exp_name} epoch{i:04d}
+        #         G val loss: {gen_val_loss:.3f}
+        #         D val loss real: {dis_val_r_loss:.3f} fake: {dis_valid_f_loss:.3f}
+        #         D val accuracy real: {dis_valid_r_acc:.2f}, fake: {dis_valid_f_acc:.2f}""".splitlines()
+        #     )
+        # )
+        # # TODO: add logging
+        # # TODO: add early stopping
+        # if i % save_interval == 0:
+        #     gen.save(f"experiments/{exp_name}/{start_time}/{i:04d}")
+        # rounding = 3
+        # # log learning curves to a CSV file.
+        # write_csv(
+        #     exp_name,
+        #     start_time,
+        #     i,
+        #     val_discriminator_accuracy_real=round(dis_valid_r_acc, rounding),
+        #     val_discriminator_loss_real=round(dis_val_r_loss, rounding),
+        #     val_discriminator_accuracy_fake=round(dis_valid_f_acc, rounding),
+        #     val_discriminator_loss_fake=round(dis_valid_f_loss, rounding),
+        # )
 
 
 def run():
@@ -306,6 +369,7 @@ def run():
     vocab_sizes = {"day": 7, "hour": 24, "category": 10}
     gen, dis, gan = build_gan(optimizer, timesteps, vocab_sizes)
     # from CSV
+    # import pandas as pd
     # df = pd.read_csv("data/dev_train_encoded_final.csv")
     # cat_frames = []
     # for key, val in vocab_sizes.items():
@@ -319,7 +383,7 @@ def run():
 
     ## From saved npy
     x = np.load("data/final_train.npy", allow_pickle=True)
-    x = x[0 : (len(vocab_sizes) + 1)]
+    # x = x[0 : (len(vocab_sizes) + 1)]
     # Padding zero to reach the maxlength
     x = np.concatenate(
         [pad_sequences(f, timesteps, padding="pre", dtype="float64") for f in x], axis=2
